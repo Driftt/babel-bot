86c2ae1bbf2762b77f2af7a217d64c20d2de378b
cbonoz <chrisdistrict@gmail.com>
2019-06-25 09:00:06 -0400

    babel bot

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const bodyParser = require('body-parser')
 const request = require('superagent');
 const Sequelize = require('sequelize');
+const { CONVERSATION_API_BASE, TRANSLATE_API_BASE, TOKEN, LANGUAGES } = require('./config')
 
 const sequelize = new Sequelize(process.env.DATABASE_URL || 'postgres://postgres:testpassword@localhost:5432/postgres')
 
@@ -39,119 +40,6 @@ const Conversation = sequelize.define('conversations', {
 Conversation.sync({
   force: true
 })
-
-
-const TRANSLATE_API_BASE = 'https://translation.googleapis.com/language/translate/v2'
-const CONVERSATION_API_BASE = process.env.QA ? 'https://driftapiqa.com/v1/conversations' : 'https://driftapi.com/v1/conversations'
-
-const LANGUAGES = {
-  "af": "Afrikaans",
-  "sq": "Albanian",
-  "am": "Amharic",
-  "ar": "Arabic",
-  "hy": "Armenian",
-  "az": "Azeerbaijani",
-  "eu": "Basque",
-  "be": "Belarusian",
-  "bn": "Bengali",
-  "bs": "Bosnian",
-  "bg": "Bulgarian",
-  "ca": "Catalan",
-  "ceb": "Cebuano",
-  "zh-CN": "Chinese (Simplified)",
-  "zh-TW": "Chinese (Traditional)",
-  "co": "Corsican",
-  "hr": "Croatian",
-  "cs": "Czech",
-  "da": "Danish",
-  "nl": "Dutch",
-  "en": "English",
-  "eo": "Esperanto",
-  "et": "Estonian",
-  "fi": "Finnish",
-  "fr": "French",
-  "fy": "Frisian",
-  "gl": "Galician",
-  "ka": "Georgian",
-  "de": "German",
-  "el": "Greek",
-  "gu": "Gujarati",
-  "ht": "Haitian Creole",
-  "ha": "Hausa",
-  "haw": "Hawaiian",
-  "iw": "Hebrew",
-  "hi": "Hindi",
-  "hmn": "Hmong",
-  "hu": "Hungarian",
-  "is": "Icelandic",
-  "ig": "Igbo",
-  "id": "Indonesian",
-  "ga": "Irish",
-  "it": "Italian",
-  "ja": "Japanese",
-  "jw": "Javanese",
-  "kn": "Kannada",
-  "kk": "Kazakh",
-  "km": "Khmer",
-  "ko": "Korean",
-  "ku": "Kurdish",
-  "ky": "Kyrgyz",
-  "lo": "Lao",
-  "la": "Latin",
-  "lv": "Latvian",
-  "lt": "Lithuanian",
-  "lb": "Luxembourgish",
-  "mk": "Macedonian",
-  "mg": "Malagasy",
-  "ms": "Malay",
-  "ml": "Malayalam",
-  "mt": "Maltese",
-  "mi": "Maori",
-  "mr": "Marathi",
-  "mn": "Mongolian",
-  "my": "Myanmar (Burmese)",
-  "ne": "Nepali",
-  "no": "Norwegian",
-  "ny": "Nyanja (Chichewa)",
-  "ps": "Pashto",
-  "fa": "Persian",
-  "pl": "Polish",
-  "pt": "Portuguese (Portugal, Brazil)",
-  "pa": "Punjabi",
-  "ro": "Romanian",
-  "ru": "Russian",
-  "sm": "Samoan",
-  "gd": "Scots Gaelic",
-  "sr": "Serbian",
-  "st": "Sesotho",
-  "sn": "Shona",
-  "sd": "Sindhi",
-  "si": "Sinhala (Sinhalese)",
-  "sk": "Slovak",
-  "sl": "Slovenian",
-  "so": "Somali",
-  "es": "Spanish",
-  "su": "Sundanese",
-  "sw": "Swahili",
-  "sv": "Swedish",
-  "tl": "Tagalog (Filipino)",
-  "tg": "Tajik",
-  "ta": "Tamil",
-  "te": "Telugu",
-  "th": "Thai",
-  "tr": "Turkish",
-  "uk": "Ukrainian",
-  "ur": "Urdu",
-  "uz": "Uzbek",
-  "vi": "Vietnamese",
-  "cy": "Welsh",
-  "xh": "Xhosa",
-  "yi": "Yiddish",
-  "yo": "Yoruba",
-  "zu": "Zulu"
-}
-
-const TOKEN = process.env.BOT_API_TOKEN
 
 app.use(bodyParser.json())
 app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'))
